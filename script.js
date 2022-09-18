d3dd1ad39d4065bd5e394093c1640c6d4b3f5f04
Aman Narang <narangaman12@gmail.com>
2022-09-18 14:58:25 +0530

    todo-list

--- script.js
+++ script.js
@@ -7,32 +7,42 @@ let text = document.getElementById("text");
 
 add.addEventListener('click', function () {
     let list = document.createElement('li');
-    let icon = document.createElement('button');
+
+    // let div=document.createElement('div');
+    // icon.style.alignSelf="center";
     // let icon1 = document.createElement('button');
-    icon.style.float="right";
-    icon.style.alignSelf="center";
-    
-    icon.innerText="Delete";
-    
     // icon1.style.float="right";
     // icon1.style.marginRight="5px";
     // icon1.innerText="Alter";
+    // list.appendChild(icon1);
+    // icon1.addEventListener('click', function () {
+    //     prompt("Please change your list", list.innerText);
+    // })
 
-    icon.value="delete";
 
+    let icon = document.createElement('button');
+    icon.style.float = "right";
+    icon.style.border = "none";
+    icon.style.backgroundColor = "white";
+    icon.style.cursor="pointer"
+
+    // icon.innerText = "Delete"; 
+    icon.className = "fa fa-trash fa-1x"
+
+    icon.value = "delete";
     list.innerText = text.value;
-    list.style.border='1px solid aliceblue ';
-    list.style.margin='5px';
-  
-   
-    list.style.padding='5px';
+    list.style.marginBottom = '5px';
+    list.style.border = '1px solid black';
+    list.style.listStyle = "none"
+
+
+    list.style.padding = '5px';
 
     if (text.value != 0) {
-        console.log(text.value);
+        // console.log(text.value);
         todolist.appendChild(list);
 
         list.appendChild(icon);
-        // list.appendChild(icon1);
 
         text.value = " ";
     }
@@ -41,38 +51,12 @@ add.addEventListener('click', function () {
         alert('Please add todo list');
     }
 
-    icon.addEventListener('click' ,function(){
+    icon.addEventListener('click', function () {
 
         list.remove();
     })
 
-    // icon1.addEventListener('click' ,function(){
-
-    //   prompt("Please change your list",list.innerText);
-    // })
 
 
-    
+
 });
-
-
-// console.log(text.value);
-// console.log(text.value);
-// todolist.appendChild(text);
-// document.body.appendChild(text);
-// list.appendChild(todolist);
-            
-// todolist.appendChild(list);
-// document.body.appendChild(list);
-           
-// let todolist = document.getElementById("todolist");
-// let box = document.getElementsByClassName("container");           
-           
-// console.log(list);
-// console.log(text.value);
-// for(let i=0;i<text.length;i++){
-//  console.log(text.value);
-            
-//     console.log(i);
-            
-// }
